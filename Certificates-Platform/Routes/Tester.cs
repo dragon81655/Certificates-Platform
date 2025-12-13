41d0c5addd30509c8d1966d0a10c897c27712c6f
dragon81655 <58427064+dragon81655@users.noreply.github.com>
2025-12-13 21:30:51 +0000

    home

---
+++ Certificates-Platform/Routes/Tester.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Certificates_Platform.Routes
+{
+       
+
+    [ApiController, Route("api")]
+    public class Tester : Controller
+    {
+        [HttpGet("test")]
+        public IActionResult Test([FromQuery]int t)
+        {
+            return Ok("API is working!" + t);
+        }
+        [HttpGet("certificate")]
+        public IActionResult Test()
+        {
+            return Ok("API is working!");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
